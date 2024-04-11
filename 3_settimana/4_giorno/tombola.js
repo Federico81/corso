b1520bc7b8791230230be770154ad7fba45a0a9b
Federico81 <federico.deambrosis@gmail.com>
2024-04-11 14:33:32 +0200

    Add files via upload

---
+++ 3_settimana/4_giorno/tombola.js
@@ -0,0 +1,20 @@
+const generateMainBoard = function () {
+    // funzione che crea il tabellone
+}
+
+const fillArray = function () {
+    // inserire in un array
+}
+
+const getRandomNum = function () {
+    // generare un numero random
+}
+
+const generateRandNumber = function () {
+    // stampare il numero
+    // associare la classe...
+}
+
+const generateUserBoards = function () {
+    // genera e gestisce le tabelline
+}
